095bb885c1050fdc0a8347cc3e4625cb2a2525f0
Fernando Mendoza <55853779+whiteknight-dev@users.noreply.github.com>
2025-05-23 18:19:35 -0600

    Setup get endpoint and default endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 
@@ -6,8 +8,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
+const tasks = [];
 
+app.get("/tasks", (request, response) => {
+  response.json(tasks);
+});
+
+app.use((request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server listen in port ${PORT}`);
 });
