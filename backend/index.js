d2c646f8e91b0c11f9a2431ee9d81e6b8905f0c4
Fernando Mendoza <55853779+whiteknight-dev@users.noreply.github.com>
2025-05-24 18:30:41 -0600

    Create delete endpoint

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const tasks = [
   {
-    id: 1,
+    id: "1",
     title: "test",
     completed: false,
   },
@@ -40,7 +40,7 @@ app.put("/tasks/:id", (request, response) => {
   const { id } = request.params;
   const { title, completed } = request.body;
 
-  const task = tasks.find((t) => t.id === Number(id));
+  const task = tasks.find((t) => t.id === id);
   if (!task) {
     return response.status(404).json({ error: "Task not found" });
   }
@@ -49,6 +49,19 @@ app.put("/tasks/:id", (request, response) => {
   if (completed !== undefined) task.completed = completed;
 
   response.json(task);
+});
+
+app.delete("/tasks/:id", (request, response) => {
+  const { id } = request.params;
+
+  const taskIndex = tasks.findIndex((t) => t.id === id);
+
+  if (taskIndex === -1) {
+    return response.status(404).json({ error: "Task not found" });
+  }
+
+  tasks.splice(taskIndex, 1);
+  response.status(204).end();
 });
 
 // Default Route
