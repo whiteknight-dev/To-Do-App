9e156f05895362eeca9421d0ff0f80986e41ffb2
Fernando Mendoza <55853779+whiteknight-dev@users.noreply.github.com>
2025-05-25 17:35:18 -0600

    Implement manage of errors

--- frontend/src/store/useTasksStore.js
+++ frontend/src/store/useTasksStore.js
@@ -1,55 +1,84 @@
 import { create } from "zustand";
 import { getApiTasks } from "../services/service";
 
+const BASE_URL = "http://localhost:3001/tasks";
+
 export const useTasksStore = create((set) => ({
   tasks: [],
   isLoading: false,
   error: null,
-  getTasks: () => {
-    const data = getApiTasks();
+  getTasks: async () => {
+    set({ isLoading: true, error: null });
 
-    set({ tasks: data });
+    try {
+      const data = await getApiTasks();
+
+      set({ tasks: data, isLoading: false });
+    } catch (e) {
+      set({ error: "Failed to fetch tasks", isLoading: false });
+    }
   },
   addTask: async ({ title }) => {
-    const res = await fetch("http://localhost:3001/tasks", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ title }),
-    });
+    set({ isLoading: true, error: null });
 
-    const newTask = await res.json();
-    set((state) => ({
-      tasks: [...state.tasks, newTask],
-    }));
+    try {
+      const res = await fetch(BASE_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title }),
+      });
+
+      const newTask = await res.json();
+      set((state) => ({
+        tasks: [...state.tasks, newTask],
+        isLoading: false,
+      }));
+    } catch (e) {
+      set({ error: "Failed to add the task", isLoading: false });
+    }
   },
   updateTask: async ({ id, title, completed }) => {
-    const res = await fetch(`http://localhost:3001/tasks/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ title, completed }),
-    });
+    set({ isLoading: true, error: null });
 
-    const newTask = await res.json();
-    set((state) => ({
-      tasks: [...state.tasks, newTask],
-    }));
+    try {
+      const res = await fetch(`${BASE_URL}/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, completed }),
+      });
+
+      const newTask = await res.json();
+      set((state) => ({
+        tasks: state.tasks.map((task) => (task.id === id ? newTask : task)),
+        isLoading: false,
+      }));
+    } catch (e) {
+      set({ error: "Failed to update the task", isLoading: false });
+    }
   },
   deleteTask: async ({ id }) => {
-    const res = await fetch(`http://localhost:3001/tasks/${id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    set({ isLoading: true, error: null });
 
-    if (res.ok) {
-      set((state) => ({
-        tasks: state.tasks.filter((task) => task.id !== id),
-      }));
+    try {
+      const res = await fetch(`${BASE_URL}/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (res.ok) {
+        set((state) => ({
+          tasks: state.tasks.filter((task) => task.id !== id),
+          isLoading: false,
+        }));
+      }
+    } catch (e) {
+      set({ error: "Failed to delete the task", isLoading: false });
     }
   },
 }));
