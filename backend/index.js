7de1c016248eabf2c4b6105ccef312b751c3dea2
Fernando Mendoza <55853779+whiteknight-dev@users.noreply.github.com>
2025-05-24 18:14:13 -0600

    Create post endpoint
    Allow us to create a to-do task.

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,40 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const crypto = require("crypto");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const tasks = [];
+const tasks = [
+  {
+    id: 1,
+    title: "test",
+    completed: false,
+  },
+];
 
 app.get("/tasks", (request, response) => {
   response.json(tasks);
 });
 
+app.post("/tasks", (request, response) => {
+  const { title } = request.body;
+  const id = crypto.randomUUID();
+
+  const newTask = {
+    id: id,
+    title: title,
+    completed: false,
+  };
+
+  tasks.push(newTask);
+  response.status(201).json(newTask);
+});
+
+// Default Route
 app.use((request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 });
